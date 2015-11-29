985609bd34029693b3865fc5bee05dc2cf9afd8b
Matthew Kuo <matthewkuo92@gmail.com>
2015-11-29 08:21:47 -0800

    evaluation revolution

--- evaluation/js/script.js
+++ evaluation/js/script.js
@@ -1,36 +1,83 @@
 $(function() {
-  var $phase1 = $('#phase1'),
-      $thanks = $('#thanks'),
-      $form = $('#phase1 form'),
-      $formButton = $form.find('button'),
-      $formFields = $form.find('input'),
-      $errorText = $('#error-text'),
-      $userPassword = $('#user-password');
+
+  var $advanceButtons = $('button.advance-button');
+  var currentPhase = 0,
+      userPassword = '',
+      phase1Count = 0;
 
   init();
 
-  function submitFormHandler(e) {
+  function advancePhase(e) {
     e.preventDefault();
 
-    var passwordsIdentical = $formFields.toArray().reduce(function(prev, current) {
-      return (prev.value === current.value && prev.value) ? prev : false;
-    });
+    if (currentPhase === 0) phase0();
 
-    if (!passwordsIdentical) {
-      $formFields.addClass('input--error');
-      $errorText.fadeIn();
-    } else {
-      $userPassword.text($formFields.get(0).value);
-      $phase1.fadeOut(function() {
-        $thanks.fadeIn();
-      });
-    }
+    $('#phase'+currentPhase).fadeOut(function() {
+      currentPhase++;
+      $('#phase'+currentPhase).fadeIn();
+
+      switch (currentPhase) {
+        case 1:
+          phase1();
+          break;
+        case 3:
+          phase3();
+          break;
+      }
+    });
+  }
+
+  /**
+   * Record user password
+   */
+  function phase0() {
+    userPassword = $('#phase0 input')[0].value;
+  }
+
+  /**
+   * User training. Has to enter password correctly 3 times in a row
+   */
+  function phase1() {
+    $form = $('#phase1 form');
+    $submit = $form.find('button');
+    $errorMessage = $form.find('.input--help.error');
+    $numTimes = $form.find('#num-times');
+
+    $('.input--help.count').show();
+    $submit.click(function(e) {
+      e.preventDefault();
+
+      var $input = $form.find('input');
+      if ($input[0].value == userPassword) {
+        $errorMessage.fadeOut();
+        $input.removeClass('input--error');
+        phase1Count++;
+      } else {
+        $errorMessage.fadeIn();
+        $input.addClass('input--error');
+        phase1Count = 0;
+      }
+
+      $numTimes.text(phase1Count);
+      $input[0].value = '';
+
+      if (phase1Count === 3) {
+        $(this).fadeOut(function() {
+          $('#phase1-advance').fadeIn();
+        });
+      }
+    });
+  }
+
+  function phase3() {
+
   }
 
   function init() {
-    $formButton.click(submitFormHandler);
-    $errorText.hide();
-    $thanks.hide();
+    // $formButton.click(submitFormHandler);
+    $('#phase1, #phase2, #phase3').hide();
+    $('#phase1-advance, .input--help').hide();
+    $advanceButtons.click(advancePhase);
   }
 
 });
